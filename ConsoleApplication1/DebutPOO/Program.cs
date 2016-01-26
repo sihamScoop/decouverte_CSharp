0d99b39466b461c6faa5eac604ef3b8827f13ad4
sihamScoop <sihem.scoop@hotmail.com>
2016-01-26 10:46:27 +0100

    modif voitures + ajout

--- ConsoleApplication1/DebutPOO/Program.cs
+++ ConsoleApplication1/DebutPOO/Program.cs
@@ -13,16 +13,17 @@ namespace DebutPOO
             //création d'une instance de la classe voiture
             Voiture voiture1 = new Voiture();
             voiture1.Klaxonner();
-            voiture1.couleur = "rouge";
+            voiture1.couleur = "rouge vermillon";
             Console.WriteLine(voiture1.couleur);
 
-            Voiture voiture2 = new Voiture();
-            voiture2.couleur = "noire";
-            Console.WriteLine(voiture2.couleur);
-
+         
             Voiture voiture3 = new Voiture();
             voiture3.couleur = "bleu";
             Console.WriteLine(voiture3.couleur);
+
+            Voiture voiture4 = new Voiture();
+            voiture4.couleur = "violet";
+            Console.WriteLine(voiture4.couleur);
 
         }
     }
