c60960977f1cbe3282b71419c70629e9888b0668
sihamScoop <sihem.scoop@hotmail.com>
2016-02-10 15:32:46 +0100

    plouf

---
+++ ConsoleApplication1/MathTests.Unit/MathTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace MathTests.Unit
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Factorielle_AvecValeur3_retourne6()
+        {
+            int valeur = 3;
+            int resultats = MaBibliothequeATester.Math.Factorielle(valeur);
+            Assert.AreEqual(6, resultats);
+        }
+
+        [Test]
+        public void Factorielle_AvecValeur10_retourne1()
+        {
+            int valeur = 10;
+            int resultats = MaBibliothequeATester.Math.Factorielle(valeur);
+            Assert.AreEqual(1, resultats,"La valeur doit être égale à 1");
+        }
+    }
+}
